f88dd58ba7608ce9ba840beb8d62d9b71a154444
Mr. Qayum <omar.qayum@tdsb.on.ca>
2022-11-22 14:44:24 -0500

    Refactored weather component to include select box

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,8 @@ const city = ref("Toronto,43.6532,79.3832");
 
 <template>
   <div class="app-container">
-    <select v-model="city">
-      <option value="Toronto,43.6532,79.3832">Toronto</option>
-      <option value="Ottawa,45.4215,75.6972">Ottawa</option>
-      <option value="Vancouver,49.2827,123.1207">Vancouver</option>
-      <option value="Calgary,51.0447,114.0719">Calgary</option>
-    </select>
     <Suspense>
-      <Weather
-        :city="city.split(',')[0]"
-        :latitude="city.split(',')[1]"
-        :longitude="city.split(',')[2]"
-      />
+      <Weather />
     </Suspense>
   </div>
 </template>
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,57 +1,40 @@
 <script setup>
 import axios from "axios";
-import { ref, watch } from "vue";
+import { ref } from "vue";
 
-const weather = ref(null);
-const props = defineProps(["city", "latitude", "longitude"]);
+const city = ref(null);
+const weather = ref(false);
 
-weather.value = (
-  await axios.get("https://api.open-meteo.com/v1/forecast", {
-    params: {
-      latitude: props.latitude,
-      longitude: props.longitude,
-      current_weather: true,
-    },
-  })
-).data;
-let condition = "";
-
-switch (weather.value.current_weather.weathercode) {
-  case 0:
-    condition = "Clear sky";
-    break;
-  case 1:
-  case 2:
-  case 3:
-    condition = "Mainly clear, partly cloudy, and overcast";
-    break;
-}
-
-watch(
-  () => props.latitude,
-  async () => {
-    weather.value = (
-      await axios.get("https://api.open-meteo.com/v1/forecast", {
-        params: {
-          latitude: props.latitude,
-          longitude: props.longitude,
-          current_weather: true,
-        },
-      })
-    ).data;
-  }
-);
+const getWeather = async () => {
+  const data = city.value.split(",");
+  weather.value = (
+    await axios.get("https://api.open-meteo.com/v1/forecast", {
+      params: {
+        latitude: data[1],
+        longitude: data[2],
+        current_weather: true,
+      },
+    })
+  ).data;
+};
 </script>
 
 <template>
   <div class="weather-container">
-    <p>{{ props.city }}</p>
-    <p>Lat: {{ weather.latitude }} deg.</p>
-    <p>Long: {{ weather.longitude }} deg.</p>
-    <p>Temp: {{ weather.current_weather.temperature }} C</p>
-    <p>Windspeed: {{ weather.current_weather.windspeed }} km/h</p>
-    <p>Wind Direction: {{ weather.current_weather.winddirection }} deg.</p>
-    <p>{{ condition }}</p>
+    <select v-model="city">
+      <option value="Toronto,43.6532,79.3832">Toronto</option>
+      <option value="Ottawa,45.4215,75.6972">Ottawa</option>
+      <option value="Vancouver,49.2827,123.1207">Vancouver</option>
+      <option value="Calgary,51.0447,114.0719">Calgary</option>
+    </select>
+    <button @click="getWeather">Get</button>
+    <div v-if="weather">
+      <p>Lat: {{ weather.latitude }} deg.</p>
+      <p>Long: {{ weather.longitude }} deg.</p>
+      <p>Temp: {{ weather.current_weather.temperature }} C</p>
+      <p>Windspeed: {{ weather.current_weather.windspeed }} km/h</p>
+      <p>Wind Direction: {{ weather.current_weather.winddirection }} deg.</p>
+    </div>
   </div>
 </template>
 
