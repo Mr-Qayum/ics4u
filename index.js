c8abeb088fae3e339f73f7d070c4ac9cd68797f8
Omar Qayum <omar.qayum@gmail.com>
2023-03-03 20:48:31 -0500

    Updated JavaScript basics with examples and comments

--- index.js
+++ index.js
@@ -1,68 +1,152 @@
-let x = 11;
-let y = 6;
+// VARIABLES
+// 1. Variables should be defined using camel case (e.g. myVariableName)
+// 2. Variables using let can be reassigned, but const variables can't
+// 3. Variables should always be declared const unless they need to be changed
+// 4. Variables are case-sensitive
+// 5. Variables using let or const cannot be redeclared with the same scope
+// 6. Arrays/objects using const can have their values modified, just not reassigned
 
-x = x + 2; // also exist for -, *, / and %
-x *= 2;  // also exist for -, *, / and %
-x++; // only for incrememnts of 1; also has ++x, x-- and --x
+// Variables using let
+let letNumber = 5; // a number
+let letFloat = 3.14; // a decimal (float)
+let letBoolean = true; // a boolean
+let letString = "Hello World!"; // a string
+let letArray = []; // an empty array
+let letObject = {}; // an empty object
 
-let firstName = "Bill";
-let lastName = "Gates";
-let fullName = `${firstName} ${lastName}`;
+// Variables using const
+const constNumber = 5; // a constant number
+const constFloat = 3.14; // a constant decimal (float)
+const constBoolean = true; // a const boolean
+const constString = "Hello World!"; // a const string
+const constArray = []; // an empty const array
+const constObject = {}; // an empty const object
 
-// console.log(firstName.toUpperCase());
-// console.log(firstName.toLowerCase());
-// console.log(fullName.split(" "));
-// console.log(fullName.includes("Bill"));
+// STRINGS
+// 1. Strings have many helper functions built into them like toUpperCase and split
+// 2. Use template literals when merging variables instead of the + operator
+// 3. When variables are added to a string, JavaScript converts them to strings
+// 4. Strings can include any character, including space, and have no size limit
 
+const firstName = "Bill";
+const lastName = "Gates";
+const fullName = firstName + " " + lastName; // the addition operator with strings means concatenation
+const fullNameTemplate = `${firstName} ${lastName}`; // this uses a template literal
+const firstNameUpper = firstName.toUpperCase();
+const firstNameLower = firstName.toLowerCase();
+const nameSplit = fullName.split(" "); // returns an array: ["Bill", "Gates"]
+
+// MATHEMATICAL OPERATORS
+// 1. Mathematical operators should only be used with integers or decimals
+// 2. JavaScript supports addition, subtraction, division, multiplication and modulus (remainder)
+// 3. Wherever possible, use x++ ++x, x-- and --x for variable incrementation or decrementation by 1
+
+let x = 5;
+x = x + 5; // adds 5 to the existing variable value and replaces it with the result
+x = x - 5; // subtracts 5 from the existing variable value and replaces it with the result
+x = x * 5; // multiplies 5 to the existing variable value and replaces it with the result
+x = x / 5; // divides 5 to the existing variable value and replaces it with the result
+x = x % 5; // mods 5 to the existing variable value and replaces it with the result
+
+// A better way to write the above code
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+
+// If you need to increment or decrement by 1
+x = x + 1; // don't do this!
+x += 1; // better but not good enough
+x++; // Perfect!
+
+// BOOLEAN OPERATORS
+// 1. Boolean operators should only be used in expressions that should result in true or false
+// 2. They can work with just about any type, if used correctly
+// 3. It is important to note that JavaScript has "truthy" and "falsy" values
+
+let a = 6;
+let b = 10;
+let c = true;
+let d = false;
+a > b; // checks if a is greater than b
+a < b; // checks if a is less than b
+a >= b; // checks if a is greater than or equal to b
+a <= b; // checks if a is less then or equal to b
+a == b; // checks if a is equal to b; if types are different JavaScript will coerce the variables
+a === b; // checks if a is equal to be and of the same type; no coercion
+c & d; // checks c AND d
+c && d; // checks c AND d, but uses short-circuit logic (i.e. if c is false, d is not checked)
+c | d; // checks c OR d
+c || d; // checks c OR d, but uses short-circuit logic (i.e. if c is true, d is not checked)
+
+// FUNCTIONS
+// 1. Functions are blocks of code that are executed upon invocation
+// 2. They can take parameters/arguments, if necessary
+// 3. They can only return a single value, if necessary
+// 4. They can be written using the "function" keyword or with arrow syntax "=>"
+
+// A Pythagorean Theorem function that takes two arguments and returns a value
 function pythagoreanTheorem1(a, b) {
   return Math.sqrt(a * a + b * b);
 }
 
+// The same function as above using arrow syntax
 const pythagoreanTheorem2 = (a, b) => {
   return Math.sqrt(a * a + b * b);
 }
 
-let c1 = pythagoreanTheorem1(2, 1);
-let c2 = pythagoreanTheorem2(2, 1);
+// Executing/calling the above functions and storing their results in variables
+const c1 = pythagoreanTheorem1(2, 1);
+const c2 = pythagoreanTheorem2(2, 1);
 
-// Arrays
-let array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let array2 = [2, 4, 6, 8, 10];
-let array3 = [...array1, ...array2];
+// ARRAYS
+// 1. Arrays can contain a set values
+// 2. Normally they contain values of the same type, but they can be mixed
+// 3. The values can be unique or duplicates
+// 4. Each value is indexed starting from 0 and ending at length - 1
+// 5. They can use the spread operator "..." and be destructured
+// 6. They have useful built-in functions like map, filter and reduce
 
-let map = array1.map((value) => {
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // an array of integers
+const array2 = [2, 4.3, "6", true, [1, 2, 3], {}]; // an array of mixed types
+const array3 = [...array1, ...array2]; // spreads the contents of array1 and array2 into a new array
+let [first, second, ...rest] = array1; // destructures the 1st, 2nd and remaining values into variables called first, second and rest, respectively
+const length = array1.length; // determines the length (size) of array1
+array1[0]; // gets the value at index 0 (first) from array1
+array1[9]; // gets the value at index 9 (last) from array1
+array1[0] = 10; // changes the value at index 0 of array1 to 10
+array1[9] = 10; // changes the value at index 9 of array1 to 10
+
+// the map function will loop through array1 and multiply all its values by 10; it returns a new array
+const map = array1.map((value) => {
   return 10 * value;
 });
 
-let filter = array1.filter((value) => {
+// the filter function will loop through array1 and only keep values that are even; it returns a new array
+const filter = array1.filter((value) => {
   return value % 2 === 0;
 });
 
-let reduce = array1.reduce((prev, cur) => {
+// the reduce function will loop through array1 and sum all the values with the sum starting at 10; it returns a new array
+const reduce = array1.reduce((prev, cur) => {
   return prev + cur;
 }, 10);
 
-// let interval = setInterval(() => {
-//   console.log("Hello class again!")
-// }, 1000);
+array1.pop(); // removes the last value in the array and returns it
+array1.push(10); // adds 10 to the end of the array
+array1.shift(); // removes the first value in the array and returns it
+array1.unshift(10); // adds 10 to the front of the array
 
-// setTimeout(() => {
-//   clearInterval(interval);
-// }, 10000);
+// OBJECTS
+// 1. Objects can store key and values as pairs
+// 2. Keys can be accessed using the "." operator or array-like syntax (e.g. objectName["keyName"])
+// 3. They can use the spread operator and be destructured
+// 4. They also have useful built-in functions like keys, values, entries and freeze
+// 5. 
 
-let [first, second, ...rest] = array1;
-
-// console.log(first);
-// console.log(second);
-// console.log(rest);
-
-array1.pop();
-array1.push(10);
-array1.shift();
-array1.unshift(10);
-
-// Objects
-let object1 = {
+// creates an object with specified key value pairs
+const object1 = {
   name: "Bill Gates",
   age: 71,
   company: "Microsoft",
@@ -71,122 +155,123 @@ let object1 = {
   cars: ["BMW", "Ferrari", "Audi"],
 };
 
-let object2 = {
+// creates another object with specified key value pairs
+const object2 = {
   address: "99 Rich Blvd.",
   hair: "white",
 }
 
-let object3 = {
+// spreads the contents of object1 and object2 into a new object
+const object3 = {
   ...object1,
   ...object2,
 }
 
-let { hair, role, cars } = object3;
-// let hair = object3.hair;
-// let role = object3.role;
+// destructures the specified keys into variables called hair, role and cars, respectively
+const { hair, role, cars } = object3;
 
-// console.log(hair, role, cars);
-// console.log(object1.name);
-// console.log(object1["name"]);
+object1.age; // gets the value of the age key
+object1.name = "Steve Jobs" // changes the value of the name key to "Steve Jobs"
+delete object1.name; // deletes the name key/value pair from the object
+object1["age"] // also gets the value of the age key using array-like syntax
+object1["name"] = "Steve Jobs" // also changes the value of the name key to "Steve Jobs"
+delete object1["name"] // also deletes the name key/value pair from the object
 
-// console.log(Object.keys(object1));
-// console.log(Object.values(object1));
-Object.freeze(object1);
-object1.name = "Steve Jobs"
-// Object.entries(object1)
-//   .forEach(([key, value]) => {
-//     console.log(`key = ${key}, value = ${value}`);
-//   });
+Object.keys(object1); // returns all the keys inside the object
+Object.values(object1); // returns all the values inside the object
+Object.entries(object1); // returns all the key/value pairs inside the object
+Object.freeze(object1); // freezes the objects values from being changed
 
-// Conditional Statements
+// CONDITIONAL STATEMENTS
+// 1. Conditional statements allow code to execute, or not execute, when one or more conditions are met
+// 2. The condition(s) should evaluate to a boolean value
 
-let age = 18;
-let citizen = true;
-let height = 149;
+const age = 18;
+const citizen = true;
+const height = 149;
+const foods = ["hamburgers", "pizzas", "meatballs", "hot dogs"]
 
-// if-else
-// if (age >= 18 && citizen && height >= 150) {
-//   console.log("You can vote!");
-// } else {
-//   console.log("You can not vote!");
-// }
+// the if-else statement is used for binary situations
+if (age >= 18 && citizen && height >= 150) {
+  console.log("You can vote!");
+} else {
+  console.log("You can not vote!");
+}
 
-// else-if
-// if (age < 18) {
-//   console.log("You don't know what you are doing!");
-// } else if (!citizen) {
-//   console.log("You don't live here!");
-// } else if (height < 150) {
-//   console.log("You need to drink milk!")
-// } else {
-//   console.log("You can vote!");
-// }
+// the ternary operator is preferred to the else-if whenever possible
+//     if (age % 2 == 0) {
+//       age *= 2;
+//     } else {
+//       age /= 2;
+//     }
+age = age % 2 == 0 ? age * 2 : age / 2; // if age is even, multiply it by 2, otherwise, divide it by 2
 
-// switch (works with strings, numbers, decimals and many others)
-let foods = ["hamburgers", "pizzas", "meatballs", "hot dogs"]
+// the else-if statement is used for non-binary situations
+if (age < 18) {
+  console.log("You don't know what you are doing!");
+} else if (!citizen) {
+  console.log("You don't live here!");
+} else if (height < 150) {
+  console.log("You need to drink milk!")
+} else {
+  console.log("You can vote!");
+}
 
-// switch (foods[4]) {
-//   case "hamburgers":
-//     console.log("I love burgers!");
-//     break;
-//   case "pizzas":
-//     console.log("I love pinapple pizza!");
-//     break;
-//   case "meatballs":
-//     console.log("I love beef!");
-//     break;
-//   case "hot dogs":
-//     console.log("I love animals too much!");
-//     break;
-//   default:
-//     console.log("Invalid food!");
-// }
+// the switch statement is preferred to the else-if statement when conditions have fixed values; it works with strings, numbers, decimals and many others
+switch (foods[4]) {
+  case "hamburgers":
+    console.log("I love burgers!");
+    break;
+  case "pizzas":
+    console.log("I love pinapple pizza!");
+    break;
+  case "meatballs":
+    console.log("I love beef!");
+    break;
+  case "hot dogs":
+    console.log("I love animals too much!");
+    break;
+  default:
+    console.log("Invalid food!");
+}
 
-// ternary operator
-// let num = 10;
+// LOOP STATEMENTS
+// 1. Loops statements allow blocks of code to execute multiple times (iterate), until some condition is met
+// 2. Loops should always modify their condition within their code block, otherwise an infinite loop will occur
 
-// if (num % 2 == 0) {
-//   num *= 2;
-// } else {
-//   num /= 2;
-// }
-
-// num = num % 2 == 0 ? num * 2 : num / 2;
-
-//console.log(num);
-
-// Loops
 let loop = 10;
 
-// while (loop > 0) {
-//   console.log(loop);
-//   loop--;
-// }
+// loops until the value of loop reaches 0; it checks the condition before first execution
+while (loop > 0) {
+  loop--;
+}
 
-// do {
-//   console.log(loop);
-//   loop--;
-// } while (loop > 0);
+// loops until the value of loop reaches 0,; it checks the condition after first execution
+do {
+  loop--;
+} while (loop > 0);
 
-// for (let i = 4; i < 10; i += 2) {
-//   console.log(i)
-// }
+// loops a finite number of times; the counter (i) starts at 4, increases by 2 each time and then stops when it reaches 10
+for (let i = 4; i < 10; i += 2) {
+  console.log(i);
+}
 
-// for (let i = 0; i < foods.length; i += 2) {
-//   console.log(foods[i]);
-// }
+// loops through the entire foods array
+for (let i = 0; i < foods.length; i++) {
+  console.log(foods[i]);
+}
 
-// Use a for-of only with arrays
-// for (const food of foods) {
-//   console.log(food);
-// }
+// a better way to loop through an array; use the for-of with arrays only
+for (const food of foods) {
+  console.log(food);
+}
 
-// Use a for-in only with objects
-// for (const item in object1) {
-//   console.log(`${item} has ${object1[item]}`);
-// }
+// another way to loop through an array using the array's built-in forEach function
+foods.forEach((food, index) => {
+  console.log(`At index=${index}, we have ${food}`);
+})
 
-// foods.forEach((food, index) => {
-//   console.log(`At index=${index}, we have ${food}`);
-// })
-
+// use the for-in with objects only
+for (const item in object1) {
+  console.log(`${item} has ${object1[item]}`);
+}
