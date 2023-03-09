ccdec9ca0a7dd64f4b5b50fd52db927fdbd01df1
Mr. Qayum <omar.qayum@tdsb.on.ca>
2023-03-09 15:13:28 -0500

    Created two components

---
+++ src/components/Form.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+
+const firstName = ref("");
+const lastName = ref("");
+const checked = ref(true);
+
+const message = () => {
+  alert(`Hello ${firstName.value} ${lastName.value}!`);
+}
+</script>
+
+<template>
+  <form @submit.prevent="message()">
+    <input v-model="firstName" type="text" placeholder="First Name" />
+    <span>{{ firstName }}</span>
+    <br />
+    <input v-model="lastName" type="text" placeholder="Last Name" />
+    <span>{{ lastName }}</span>
+    <br />
+    <input type="checkbox" v-model="checked"/> Happy
+    <br />
+    <input type="submit" value="Click"/>
+  </form>
+  {{ checked }}
+</template>
+
+<style scoped></style>
---
+++ src/components/OptimusPrime.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import {ref} from "vue";
+
+  const isVisible = ref(true);
+  const count = ref(0);
+
+  const hide = () => {
+    isVisible.value = !isVisible.value;
+    if (!isVisible.value) {
+      count.value++;
+    }
+  }
+</script>
+
+<template>
+  <div id="main">
+    <p>{{ count }}</p>
+    <button @click="hide()">Hide</button>
+    <img v-if="isVisible" class="optimus"
+      src="https://cdn.shopify.com/s/files/1/0054/4371/5170/products/667pin.png?v=1627414689" />
+  </div>
+</template>
+
+<style scoped>
+#main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.optimus {
+  width: 25%;
+  aspect-ratio: 2 / 3;
+}
+</style>
