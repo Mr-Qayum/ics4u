b72c6a5dab99c3eed1d731a7de51ddebca3ff774
Mr. Qayum <omar.qayum@tdsb.on.ca>
2023-05-01 15:15:35 -0400

    Added firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDy_jxTa9P18N2qpvycR0_biIqPMyOIkjE",
+  authDomain: "vue-project-c422e.firebaseapp.com",
+  projectId: "vue-project-c422e",
+  storageBucket: "vue-project-c422e.appspot.com",
+  messagingSenderId: "150837332711",
+  appId: "1:150837332711:web:5c09dc3b21d20e199e3042"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
