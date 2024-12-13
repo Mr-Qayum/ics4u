026e51340d420758919e3ad870857bb805ca78d8
Omar Qayum <omar.qayum@tdsb.on.ca>
2024-12-13 11:03:09 -0500

    Fixed page refresh issue

--- 4. React/6. Summative/src/context/index.jsx
+++ 4. React/6. Summative/src/context/index.jsx
@@ -1,19 +1,33 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 import { Map } from 'immutable';
+import { auth } from "../firebase";
 
 const StoreContext = createContext();
 
 export const StoreProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const [cart, setCart] = useState(Map());
+  const [user, setUser] = useState(null);
+  const [cart, setCart] = useState(Map());
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <StoreContext.Provider value={{ user, setUser, cart, setCart }}>
-            {children}
-        </StoreContext.Provider>
-    );
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      setUser(user);
+      setLoading(false);
+    });
+  }, [])
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
+  return (
+    <StoreContext.Provider value={{ user, setUser, cart, setCart }}>
+      {children}
+    </StoreContext.Provider>
+  );
 }
 
 export const useStoreContext = () => {
-    return useContext(StoreContext);
+  return useContext(StoreContext);
 }
