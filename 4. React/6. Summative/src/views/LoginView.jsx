7623da8c060d233f8c66c6ee571167459cced523
Omar Qayum <omar.qayum@tdsb.on.ca>
2024-12-13 10:32:59 -0500

    Added firebase auth to register page

--- 4. React/6. Summative/src/views/LoginView.jsx
+++ 4. React/6. Summative/src/views/LoginView.jsx
@@ -3,21 +3,36 @@ import './LoginView.css';
 import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useStoreContext } from '../context';
+import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { auth } from '../firebase';
 
 function LoginView() {
-
-  const [password, setPassword] = useState('');
   const email = useRef('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const { setEmail } = useStoreContext();
+  const { setUser } = useStoreContext();
 
-  function login(event) {
+  async function loginByEmail(event) {
     event.preventDefault();
-    if (password === "iloveyou") {
+
+    try {
+      const user = (await signInWithEmailAndPassword(auth, email.current.value, password)).user;
       navigate('/movies/all');
-      setEmail(email.current.value);
-    } else {
-      alert("Wrong password!");
+      setUser(user);
+    } catch (error) {
+      console.log(error);
+      alert("Error signing in!");
+    }
+  }
+
+  async function loginByGoogle() {
+    try {
+      const user = (await signInWithPopup(auth, new GoogleAuthProvider())).user;
+      navigate('/movies/all');
+      setUser(user);
+    } catch (error) {
+      console.log(error);
+      alert("Error signing in!");
     }
   }
 
@@ -25,15 +40,16 @@ function LoginView() {
     <div className="login-container">
       <div className="form-container">
         <h2>Login to Your Account</h2>
-        <form onSubmit={(event) => { login(event) }}>
+        <form onSubmit={(event) => { loginByEmail(event) }}>
           <label htmlFor="email">Email</label>
           <input type="email" id="email" name="email" ref={email} required />
 
           <label htmlFor="password">Password</label>
           <input type="password" id="password" name="password" value={password} onChange={(event) => { setPassword(event.target.value) }} required />
 
-          <button type="submit" className="login-button">Login</button>
+          <button type="submit" className="login-button">Login by Email</button>
         </form>
+        <button onClick={() => loginByGoogle()} type="submit" className="login-button">Login by Google</button>
         <p className="register-link">New to Netflix? <a href="#">Register now</a></p>
       </div>
     </div>
